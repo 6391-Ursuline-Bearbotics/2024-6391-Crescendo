580e322ee1dec8b969488eef6d5a27be6ccdcf6d
6391-Bearbotics <uarobotics@ursulinestl.org>
2024-03-20 10:23:05 -0500

    int table updates

--- src/main/java/frc/robot/Util/InterpolatingTable.java
+++ src/main/java/frc/robot/Util/InterpolatingTable.java
@@ -10,10 +10,10 @@ import static java.util.Map.entry;
 
 // Interpolating table
 public class InterpolatingTable {
-    public static final ShotParameter sub = new ShotParameter(10.0, 125); //8.0 100
-    public static final ShotParameter auto = new ShotParameter(31.0, 140); //27
-    public static final ShotParameter stage = new ShotParameter(43.5, 200); //42
-    public static final ShotParameter wing = new ShotParameter(49.2, 220); // 48.2
+    public static final ShotParameter sub = new ShotParameter(11, 125); //8.0 100
+    public static final ShotParameter auto = new ShotParameter(31.2, 140); //27
+    public static final ShotParameter stage = new ShotParameter(41.5, 200); //42
+    public static final ShotParameter wing = new ShotParameter(53, 220); // 48.2
     public static final ShotParameter farwing = new ShotParameter(38.3, 142); //36.3
 
     /* Private constructor because this is a utility class */
@@ -22,10 +22,10 @@ public class InterpolatingTable {
     // Interpolating tree map
     private static final TreeMap<Double, ShotParameter> map = new TreeMap<>(
         Map.ofEntries(
-            entry(1.20, sub),
-            entry(2.21, auto),
-            entry(3.3, stage),
-            entry(6.24, wing),
+            entry(1.15, sub),
+            entry(2.08, auto),
+            entry(3.39, stage),
+            entry(6.20, wing),
             entry(10.18, farwing)
         )
     );
